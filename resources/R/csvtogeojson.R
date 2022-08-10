6541c159404493964ca54897b33fe2e24a8f48d0
Diana Mogrovejo <keydjahna@gmail.com>
2022-08-10 10:08:46 +0100

    cambio de titulo

--- resources/R/csvtogeojson.R
+++ resources/R/csvtogeojson.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(geojsonio)
 library(sf)
-data <- read_csv("csv/StoryMap.csv") %>% 
+data <- read_csv("resources/csv/StoryMap.csv") %>% 
   st_as_sf(coords = c("lon","lat"), crs = 4326)
 geojson_write(
   data,
