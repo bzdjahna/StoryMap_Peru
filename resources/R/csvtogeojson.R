c1695b230b51681098863d48e84de17a97eba435
bzdjahna <bzdjahna@users.noreply.github.com>
2022-08-10 22:53:02 +0000

    deploy: 54ed738d539ab806a77f46d2c66d9bf6f97100a1

--- resources/R/csvtogeojson.R
+++ resources/R/csvtogeojson.R
@@ -1,10 +1,11 @@
-library(tidyverse)
 library(geojsonio)
 library(sf)
-data <- read_csv("resources/csv/StoryMap.csv") %>% 
+data <- read.csv("resources/csv/StoryMap.csv",
+  encoding = "latin1") %>% 
   st_as_sf(coords = c("lon","lat"), crs = 4326)
 geojson_write(
   data,
   file = "StoryMap.geojson"
   )
+
 
